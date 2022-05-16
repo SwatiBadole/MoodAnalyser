3826f708b2d07314d63f0e222fd5c6ed61af707f
SwatiBadole <swatibadole1997@gmail.com>
2022-05-16 14:51:27 -0700

    Added:Mood message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,13 +1,31 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
-        if(message.contains("Sad")) {
-            return "SAD";
+
+    public static String message;
+
+    // Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+        analyseMood("User is Sad");
+    }
+
+    //Getter
+    public String getMessage() {
+        return message;
+    }
+
+    //Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String analyseMood(String user_is_sad) {
+
+        if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("happy")) {
         }
-        else if (message.contains("Happy")) {
-            return "HAPPY";
-        }
-        return null;
+        return "Happy";
     }
 }
+
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
         String result = moodAnalyser.analyseMood("User is Sad");
         Assertions.assertEquals(result, "SAD");
     }
     @Test
     public void givenMessage_WhenProper_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Happy Mood");
         String result=moodAnalyser.analyseMood("User is Happy");
         Assertions.assertEquals(result,"HAPPY");
     }
